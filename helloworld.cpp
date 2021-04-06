dc2ffa152e16be135063e02dd370c3cf68bf2f76
MADHU GUNNEBOINA <madhuyadav6060@gmail.com>
2021-04-06 07:53:54 +0530

    Implemented helloworld program

---
+++ helloworld.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // cout<<"Hello World\n";
+    // return 0;
+    int amount1;
+    cin>>amount1;
+
+    int amount2;
+    cin>>amount2;
+
+    int sum=amount1+amount2;
+    cout<<sum<<endl;
+
+    return 0;
+
+}
